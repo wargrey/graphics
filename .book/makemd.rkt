846b06e29c6fcae9ef98c45de6ff2e0ff78a230d
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-10-23 22:01:47 +0800

    Update Mission: makemd.rkt: Refactor with Pattern Match

--- .book/makemd.rkt
+++ .book/makemd.rkt
@@ -15,7 +15,7 @@ cd $(dirname $0) && exec racket $(basename $0);
 (define rootdir (simplify-path (build-path 'up)))
 (define bookdir (path->complete-path (current-directory)))
 (define rsttdir (build-path bookdir "island" "rosetta"))
-(define makemd (build-path bookdir "makemd.rkt"))
+(define make.md (build-path bookdir "makemd.rkt"))
 
 (define make-markdown
   {lambda [target dentry]
@@ -25,32 +25,32 @@ cd $(dirname $0) && exec racket $(basename $0);
                     (path-replace-suffix (file-name-from-path md) #"") dentry))
     (with-output-to-file target #:exists 'replace
       {thunk (define awkout (current-thread))
-             (define awk-format (thread {thunk (define pipen awkout)
-                                               (let awk ()
-                                                 (define line (thread-receive))
-                                                 (cond [(eof-object? line) (thread-send pipen eof)]
-                                                       [(regexp-match? #px"^#+ (\\d+\\.)+" line) (thread-send pipen (regexp-replace #px"^(#+) (\\d+\\.)+" line "\\1"))]
-                                                       [(regexp-match? #px"^\\s*$" line) (let ([next (thread-receive)])
-                                                                                           (cond [(and (not (eof-object? next))
-                                                                                                       (regexp-match? #px"(^\\* )|(^\\s*$)" next)) 'Skip-Empty-Line]
-                                                                                                 [else (thread-send pipen line)])
-                                                                                           (thread-rewind-receive (list next)))]
-                                                       [(regexp-match? #px"^\\* >\\s+>" line) (thread-send pipen (regexp-replace #px"^\\* >(\\s+)>" line "\\1- "))]
-                                                       [else (thread-send pipen line)])
-                                                 (unless (eof-object? line)
-                                                   (awk)))}))
-             (call-with-input-file md
-               {lambda [markdown.md]
-                 (define pipe0 awk-format)
-                 (let awk-readme ([waitees (list markdown.md (thread-receive-evt))])
-                   (define which (apply sync waitees))
-                   (cond [(eq? markdown.md which) (let ([line (read-line markdown.md)])
-                                                   (cond [(eof-object? line) (thread-send pipe0 line) (awk-readme (list (thread-receive-evt)))]
-                                                         [else (thread-send pipe0 line) (awk-readme waitees)]))]
-                         [(eq? (thread-receive-evt) which) (let ([line (thread-receive)])
-                                                             (unless (eof-object? line)
-                                                               (displayln line)
-                                                               (awk-readme waitees)))]))})})})
+             (define awk-format (thread {thunk (let awk ([pipen awkout])
+                                                 (define in (thread-receive))
+                                                 (define out (match in
+                                                               [{pregexp #px"^#+ (\\d+\\.)+"} (regexp-replace #px"^(#+) (\\d+\\.)+" in "\\1")]
+                                                               [{pregexp #px"^\\* >\\s+>"} (regexp-replace #px"^\\* >(\\s+)>" in "\\1- ")]
+                                                               [{pregexp #px"\\]\\(~/"} (string-replace in "~" (substring (~a rsttdir) (sub1 (string-length (~a rootdir)))))]
+                                                               [{pregexp #px"^\\s*$"} (let ([next (thread-receive)])
+                                                                                        (cond [(and (not (eof-object? next))
+                                                                                                    (regexp-match? #px"(^\\* )|(^\\s*$)" next)) 'Skip-Empty-Line]
+                                                                                              [else (thread-send pipen "")])
+                                                                                        (thread-rewind-receive (list next)))]
+                                                               [_ in]))
+                                                 (when (string? out) (thread-send pipen out))
+                                                 (if (eof-object? out) (thread-send pipen eof) (awk pipen)))}))
+             (with-input-from-file md
+               {thunk (let awk-readme ([pipe0 awk-format] [waitees (list (current-input-port) (thread-receive-evt))])
+                        (define waiteen (if (input-port? (apply sync waitees))
+                                            (match (read-line)
+                                              [{? eof-object?} {begin (thread-send pipe0 eof) (list (thread-receive-evt))}]
+                                              [{var line} {begin (thread-send pipe0 line) waitees}])
+                                            (let ([line (thread-receive)])
+                                              (unless (eof-object? line)
+                                                (displayln line)
+                                                waitees))))
+                        (when (list? waiteen)
+                          (awk-readme pipe0 waiteen)))})})})
 
 (define make-png
   {lambda [target dentry]
@@ -76,10 +76,18 @@ cd $(dirname $0) && exec racket $(basename $0);
 
 (define readmes (filter list? (for/list ([readme.scrbl (in-directory rsttdir)]
                                          #:when (string=? (path->string (file-name-from-path readme.scrbl)) "readme.scrbl"))
-                                (with-handlers ([exn:fail:contract? {lambda [efc] (fprintf (current-error-port) "~a" efc)}])
+                                (with-handlers ([exn:fail:contract? values])
                                   (define t (reroot-path (substring (path->string (path-replace-suffix readme.scrbl #".md")) (string-length (path->string rsttdir))) rootdir))
-                                  (define ds (cons readme.scrbl (remove readme.scrbl (smart-dependencies readme.scrbl))))
+                                  (define ds (cons readme.scrbl (cons make.md (remove readme.scrbl (smart-dependencies readme.scrbl)))))
                                   (list t ds {thunk (make-markdown t readme.scrbl)})))))
 
 (unless (null? readmes)
   (make/proc readmes (map car readmes)))
+
+#|
+(define images (filter list? (foldl append (for/list ([readme.md (map car readmes)])
+                                             (with-handlers ([exn:fail:contract? {lambda [efc] (eprintf "~a" efc) null}])
+                                               (define images (regexp-match*  #px"(?<=]\\()/.book/island/" readme.md))
+                                               (define ds (cons readme.scrbl (remove readme.scrbl (smart-dependencies readme.scrbl))))
+                                               (list t ds {thunk (make-markdown t readme.scrbl)}))))))
+|#
