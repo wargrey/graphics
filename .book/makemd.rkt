351a4906870d08642acbeaf8444e53cf4990aae0
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-11-07 14:06:03 +0800

    Fixup Mission: makemd.rkt: he hiragana -> katakana

--- .book/makemd.rkt
+++ .book/makemd.rkt
@@ -29,7 +29,7 @@ cd $(dirname $0) && exec racket $(basename $0);
                     'サ 'sa 'シ 'shi 'ス 'su 'セ 'se 'ソ 'so 'ザ 'za 'ジ 'ji 'ズ 'zu 'ゼ 'ze 'ゾ 'zo
                     'タ 'ta2 'チ 'chi 'ツ 'tsu 'テ 'te 'ト 'to 'ダ 'da 'デ 'de 'ド 'do
                     'ナ 'na 'ニ 'ni 'ヌ 'nu 'ネ 'ne 'ノ 'no
-                    'ハ 'ha 'ヒ 'hi3 'フ 'fu 'へ 'he 'ホ 'ho 'バ 'ba 'ビ 'bi 'ブ 'bu 'ベ 'be 'ボ 'bo 'パ 'pa 'ピ 'pi3 'プ 'pu 'ペ 'pe 'ポ 'po
+                    'ハ 'ha 'ヒ 'hi3 'フ 'fu 'ヘ 'he 'ホ 'ho 'バ 'ba 'ビ 'bi 'ブ 'bu 'ベ 'be 'ボ 'bo 'パ 'pa 'ピ 'pi3 'プ 'pu 'ペ 'pe 'ポ 'po
                     'マ 'ma2 'ミ 'mi 'ム 'mu 'メ 'me 'モ 'mo
                     'ラ 'ra 'リ 'ri 'ル 'ru2 'レ 're 'ロ 'ro))
 
