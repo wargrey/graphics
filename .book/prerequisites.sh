1c317c5870f2b33f74cd06953637e64cffb63a9a
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-11-23 13:08:54 +0800

    Update Mission: Sync prerequisites.sh

--- .book/prerequisites.sh
+++ .book/prerequisites.sh
@@ -6,7 +6,7 @@ exit $?;
 
 ######################## Makefile #######################
 SHELL := $(shell which bash) -c -x
-.ALLPHONY := racket
+.ALLPHONY := racket swipl
 
 export OSTYPE := $(shell uname -s)
 export OSNAME := $(shell perl -e 'print $$^O;')
@@ -21,6 +21,9 @@ unexport MAKEFLAGS
 
 export CPUS := 4
 export CC := gcc
+# LDFLAGS CPPFLAGS are set for MacOSX
+export LDFLAGS := -L/usr/local/opt/readline/lib -L/usr/local/opt/gmp/lib
+export CPPFLAGS := -I/usr/cal/opt/readline/include -I/usr/local/opt/gmp/include
 
 Prefix := /opt
 VerRegExp := \([0-9]\+\.\)\+[0-9]\+
@@ -32,6 +35,12 @@ racket_src := racket-minimal-$(racket_verion)-src-builtpkgs.tgz
 racket_pkgs := math html make images
 racket_config_darwin := --enable-macprefix
 racket_config_linux :=
+
+prolog_dir := GYDMprolog
+prolog_bin := $(shell swipl_bin=`which swipl`; test $$? && echo $${swipl_bin} || echo $(Prefix)/$(prolog_dir)/bin/swipl)
+prolog_version := $(shell curl http://www.swi-prolog.org/download/devel | grep -m 1 -o 'download/devel/src/pl-$(VerRegExp)' | tr -d 'a-z-/')
+prolog_src := pl-$(prolog_version).tar.gz
+prolog_skipped_pkgs := R odbc xpce jpl PDT jasmine
 
 $(racket_src):
 	curl -O "http://mirror.racket-lang.org/installers/$(racket_verion)/$(racket_src)"
@@ -45,6 +54,22 @@ $(Prefix)/$(racket_dir)/bin/racket: $(racket_src)
 racket: | $(racket_bin)
 	sudo $(addprefix $(dir $(racket_bin)),raco) pkg install --skip-installed --scope installation --deps search-auto $(racket_pkgs)
 	$(racket_bin) -l racket/base -e '(printf "We have Racket ~a.~n" (version))'
+
+$(prolog_src):
+	curl -O http://www.swi-prolog.org/download/devel/src/$(prolog_src)
+
+$(Prefix)/$(prolog_dir)/bin/swipl: $(prolog_src)
+	rm -fr pl-$(prolog_version) && tar xzvf $(prolog_src)
+	cd pl-$(prolog_version) && ./prepare --yes --all
+	cd pl-$(prolog_version) && ./configure --prefix=$(Prefix)/$(prolog_dir) --enable-shared --enable-usrprofile --with-world $(addprefix --without-,$(prolog_skipped_pkgs))
+	cd pl-$(prolog_version) && $(MAKE) -j$(CPUS)
+	cd pl-$(prolog_version) && sudo $(MAKE) install
+
+swipl: | $(prolog_bin)
+	echo "main :-" > display-version.pl
+	echo " 	current_prolog_flag(version_data, swi(M, I, P, []))," >> display-version.pl
+	echo " 	format('We have SWI-Prolog ~w.~w.~w.\n', [M, I, P])." >> display-version.pl
+	$(prolog_bin) -g main -s display-version.pl -t halt
 
 ### vim:ft=make
 
