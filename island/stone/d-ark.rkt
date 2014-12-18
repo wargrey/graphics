8fd0fba45497304af5d6dbb7c1812a3f675d701a
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-12-18 18:03:27 +0800

    Update Mission: d-ark.rkt: Support auto-close

--- island/stone/d-ark.rkt
+++ island/stone/d-ark.rkt
@@ -6,10 +6,10 @@
 (provide (all-from-out "../../village/sakuyamon/digitama/digimon.rkt"))
 
 (define digimon-ark
-  {lambda [monster #:lightness [threshold 0.64] #:rallies [rallies0 null]]
+  {lambda [monster #:lightness [threshold 0.64] #:rallies [rallies0 null] #:close [close? 0]]
     (define-values {width0 height0} (flomap-size (cdr (digimon-figure monster))))
     (define figure (flomap-copy (cdr (digimon-figure monster)) 0 0 width0 height0))
-    (define d-ark (new {class dialog% (super-new [label "Digimon Analyzer"] [style '{close-button}] [min-width width0] [min-height height0])
+    (define d-ark (new {class dialog% (super-new [label (format "Analyzer [~a, ~a]" width0 height0)] [style '{close-button}] [min-width width0] [min-height height0])
                          (field [rallies null] [ltxy #false])
                          (field [vark (new canvas% [parent this] [paint-callback {lambda [who-cares painter] (send painter draw-bitmap (flomap->bitmap figure) 0 0)}])])
                          (field [dark (let ([arc (send vark get-dc)])
@@ -38,7 +38,10 @@
                                                 (when (void? (erase (car rally) (cdr rally)))
                                                   (printf "WARNING: (cons ~a ~a) is reduntant~n" (car rally) (cdr rally)))}
                                               (cons (cons 0 0) rallies0))
-                                    (send vark refresh-now)]
+                                    (send vark refresh-now)
+                                    (when (and (real? close?) (>= close? 0))
+                                      (sleep close?)
+                                      (send this show #false))]
                                  [else (for-each {lambda [sally] (printf "(cons ~a ~a)~n" (car sally) (cdr sally))} rallies)]))
 
                          (define/override (on-subwindow-event who mouse)
