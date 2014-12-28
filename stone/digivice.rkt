226138213c380ab09bf1d87ff7ce6e2db1bafecd
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2014-12-29 00:28:17 +0800

    Fixup stone: digivice.rkt: Fixup paths relative to digivice dir

--- stone/digivice.rkt
+++ stone/digivice.rkt
@@ -7,14 +7,14 @@
 (define show-help-and-exit : {[#:exitcode Integer] [#:errcmd (Option String)] -> Void}
   {lambda [#:exitcode [exitcode 0] #:errcmd [error-command #false]]
     (define printf0 : {String Any * -> Void} (if error-command eprintf printf))
-    (define cmds : (Listof String) (map path->string (filter ((inst curry Regexp Boolean Path) regexp-match? #px".rkt$")
-                                                             (directory-list))))
+    (define cmds : (Listof String) (map path->string (filter ((inst curry Regexp Boolean Path) regexp-match? #px"(?<!@|digivice|).rkt$")
+                                                             (directory-list "@|digivice|-ark"))))
     (define width : Natural (string-length (argmax string-length cmds)))
     (printf0 "Usage: @|digivice| <command> [<option> ...] [<arg> ...]~n~nAll available commands:~n")
     (for ([cmd : String (in-list cmds)])
       (printf0 "  ~a ~a~n" (~a (regexp-replace #px"^(.+).rkt$" cmd "\\1") #:min-width width)
                (car ((inst call-with-values (Listof Any))
-                     {lambda [] (dynamic-require cmd (cast 'desc Symbol) {lambda [] : Any "[Mission Description]"})} list))))
+                     {lambda [] (dynamic-require (format "@|digivice|-ark/~a" cmd) (cast 'desc Symbol) {lambda [] : Any "[Mission Description]"})} list))))
     (when error-command
       (printf0 "~n@|digivice|: Unrecognized command: ~a~n"
                (build-path (find-system-path 'orig-dir) (find-system-path 'run-file)) error-command))
