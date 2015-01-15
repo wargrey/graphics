bc400f65ec2df3a45324413461a4e0b3508ed23a
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-01-15 21:59:25 +0800

    Update digicore: Add missing info.rkt and skip all since the required sources will be compiled automatically

--- info.rkt
+++ info.rkt
@@ -5,4 +5,4 @@
 (define compile-omit-paths 'all)
 (define test-omit-paths 'all)
 
-(define setup-collects (list "digicore" "sakuyamon" "nanomon"))
+(define setup-collects (list "sakuyamon" "nanomon"))
