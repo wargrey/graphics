f35cb10fcaaa1401f0f5bdd644354d547ffdc0fb
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-01-21 22:56:58 +0800

    Update digicore: tamer.rkt: Use (fold-test-results) to output with styles of hspec and harness

--- DigiGnome/tamer/makefile.rkt
+++ DigiGnome/tamer/makefile.rkt
@@ -18,10 +18,30 @@ So I@literal{'}m glad to help you to ensure that the
                             [{list 'file file} (simplify-path (build-path (path-only (syntax-source #'makefile)) file))]))]{makefile.rkt}
 works as you wish.
 
-@chunk[<*:outline>
-       {module story racket
-         <ready?-help!>
-         <hello-rules!>}]
+Although normal @bold{Racket} script doesn@literal{'}t require the so-called @racketidfont{main} routine,
+I still prefer to start with @defproc[{main [argument string?] ...} void?]
+
+and the story starts with @chunk[<*:outline>
+                                 {module story racket
+                                   <tamer-routine>
+                                   
+                                   <ready?-help!>
+                                   <hello-rules!>}
+                                                  
+                                 <tamer-spec>]
+
+where @chunk[<tamer-routine>
+             (require "tamer.rkt")
+             (require (submod "tamer.rkt" makefile))
+           
+             (define make (dynamic-require tamer-partner 'main (const #false)))]
+
+and @chunk[<tamer-spec>
+           {module test racket
+             (require "tamer.rkt")
+             (require (submod "tamer.rkt" makefile))
+             
+             (tamer-spec)}]
 
 @margin-note{@racket[subsection]s: Each of them describes a scenario.}
 @subsection{Scenario: Ready? Let@literal{'}s have a try!}
@@ -32,20 +52,8 @@ works as you wish.
                                                                                              @item{Expected Outcome})}
 
 @chunk[<ready?-help!>
-       <tamer-routine>
-       <ready?-suites>]
-
-Although normal @bold{Racket} script doesn@literal{'}t require the so-called @racketidfont{main} routine,
-I still prefer to start with the @racketidfont{main} in production. So the @tamer-action[tamer-partner] should
-provide the @racketidfont{main} as its entry: @defproc[{main [argument string?] ...} void?]
-
-To make it more meaningful
-the @chunk[<tamer-routine>
-           (require "tamer.rkt")
-           (require (submod "tamer.rkt" makefile))
-           
-           (define make (dynamic-require tamer-partner 'main (const #false)))]
-make the @racketidfont{make} as the alias for @racketidfont{main}.
+       <testsuite:option-ready?>
+       <testsuite:goal-not-ready!>]
 
 You may be already familiar with the @hyperlink["http://en.wikipedia.org/wiki/Make_(software)"]{GNU Make},
 nonetheless you are still free to check the options first. Normal @bold{Racket} program always knows
@@ -56,47 +64,39 @@ nonetheless you are still free to check the options first. Normal @bold{Racket} 
               (code:comment @#,t{See, @racketcommentfont{@italic{makefile}} complains that @racketcommentfont{@bold{Scribble}} is killed by accident.})
               (code:comment @#,t{Meanwhile parallel building is not supported.})]
 
-Now it@literal{'}s time to interact with @chunk[<ready?-suites>
-                                                <suite:option-ready?>
-                                                <suite:goal-not-ready!>]
-to look deep into the specification templates:
-
-@itemlist{@item{An example of the testsuite that should pass and do pass:}}
+Now it@literal{'}s time to look deep into the specification examples of
+@itemlist{@item{a testsuite that should pass and do pass:}}
 @tamer-note['option-ready?]
-@chunk[<suite:option-ready?>
+@chunk[<testsuite:option-ready?>
        (define option-ready?
-         (test-suite "makefile.rkt [maybe option]"
-                     (test-case "should exit normally"
-                                (check-$? {thunk (make "--help")} 0))
-                     (test-case "should abort if facing unknown option"
-                                (check-$? {thunk (make "--undef")} 1))
-                     (test-case "should say nothing unless errors ocurr"
-                                (let ([stdout (open-output-string 'stdout)]
-                                      [stderr (open-output-string 'stderr)])
-                                  (parameterize ([current-output-port stdout]
-                                                 [current-error-port stderr]
-                                                 [exit-handler void])
-                                    (make "--silent" "--help")
-                                    (make "--silent" "--undefined"))
-                                  (let ([strout (get-output-string stdout)]
-                                        [strerr (get-output-string stderr)])
-                                    (check-equal? strout "")
-                                    (check-not-equal? strerr ""))))))]
+         (test-suite "make [option]"
+                     (test-suite "make --help"
+                                 (test-$? "should exit normally"
+                                          {thunk (make "--help")} 0))
+                     (test-suite "make [unknown option]"
+                                 (test-$? "should abort"
+                                          {thunk (make "--unknown")} 1))
+                     (let ([words (stdpipe->cons {thunk (make "-s" "--help")
+                                                        (make "-s" "--say")})])
+                       (test-suite "make --silent"
+                                   (test-case "should say nothing but errors"
+                                              (check-regexp-match
+                                               #rx"^\\( \\. .+?\\)$"
+                                               (format "~a" words)))))))]
 @tamer-action[tamer-script
               (tamer-script 'option-ready?)]
 
-@itemlist{@item{An example of the testsuite that should pass but do fail:}}
+@itemlist{@item{a testsuite that should pass but do fail:}}
 @tamer-note['goal-not-ready!]
-@chunk[<suite:goal-not-ready!>
+@chunk[<testsuite:goal-not-ready!>
        (define goal-not-ready!
-         (test-suite "makefile.rkt [maybe target]"
-                     (test-case "should abort if using default exit-handler"
-                                (check-not-exn {thunk (make "love")}
-                                               "Scribble is killed!"))))]
-@tamer-action[(tamer-script 'goal-not-ready!)
-              (code:comment @#,t{See. @racketcommentfont{@bold{Racket/Scribble}} has no idea about the broken lines.})]
+         (test-suite "make [phony target]"
+                     (test-suite "make love"
+                                 (test-not-exn "should catch a fatal to regain control"
+                                               {thunk (make "love")}))))]
+@tamer-action[(tamer-script 'goal-not-ready!)]
 
-@itemlist{@item{An example of the typo that should never happen:}}
+@itemlist{@item{a typo that should never happen:}}
 @tamer-note['maybe-typo!]
 @tamer-action[(tamer-script 'maybe-typo!)]
 
@@ -105,12 +105,45 @@ writing the @italic{handbook} becomes fantastic. We can @racket[eval] the code w
 @racket[render]ing the documents along with it. Since the two ways,
 via @racket[margin-note](@racket[tamer-note]) and via @racket[interaction](@racket[tamer-action]),
 are just duplicate work, there is no need to run them at the same @italic{handbook}.
-As examples, I just show you the effects of the two ways, then you can choose your favor.
+I just show you the effects of the two ways, then you can choose your favor.
 
 @subsection{Scenario: The rules serve you!}
 
-Since @italic{Behavior Driven Development} is the evolution of @italic{Test Driven Development} which doesn@literal{'}t define
-what exactly should be tested and how would the tests be performed correct.
-So we say @chunk[<hello-rules!>
-                 (void)]
-that apart from the conventions.
+@chunk[<hello-rules!>
+       <project-hierarchy>]
+
+Since @italic{Behavior Driven Development} is the evolution of @italic{Test Driven Development} which does not define
+what exactly should be tested and how would the tests be performed correct. The term @italic{Architecture} is all about designing
+rules, and this story is all about building system. So apart from conventions, we need a sort of rules that the @italic{makefile.rkt}
+(and systems it builds) should satisfy.
+
+@(itemlist @item{@bold{Rule 1}: The entire project is a @italic{multi-collection package},
+                  and (nonhidden) directories within it are considered as subprojects.}
+           @item{@bold{Rule 2}: The subproject should have an explicit name,
+                  although the name can be the same as its directory name.})
+
+@tamer-note['rule-1 'rule-2]
+@chunk[<project-hierarchy>
+       (require setup/getinfo)
+       
+       (define digimons (filter {lambda [sub] (and (directory-exists? sub)
+                                                   (regexp-match? #px"^[^.]" sub))}
+                                (directory-list rootdir)))
+       (define info-root (get-info/full rootdir))
+       (define rule-1
+         (test-suite "Rule 1"
+                     (test-suite "Look inside the `/info.rkt`"
+                                 (test-case "'collection should be 'multi"
+                                            (check-not-false info-root)
+                                            (check-equal? (info-root 'collection) 'multi)))))
+       (define rule-2
+         (make-test-suite "Rule 2"
+                          (map {lambda [digimon]
+                                 (define info-ref (get-info/full (build-path rootdir digimon)))
+                                 (test-suite (format "Look inside the `/~a/info.rkt`" digimon)
+                                             (test-case (format "'collection should be string" digimon)
+                                                        (check-not-false info-ref)
+                                                        (check-pred string? (info-ref 'collection))))}
+                               digimons)))]
+
+@tamer-action[(tamer-script 'rule-2)]
--- digicore/digitama/tamer.rkt
+++ digicore/digitama/tamer.rkt
@@ -1,7 +1,6 @@
 #lang racket
 
 (require rackunit)
-(require rackunit/text-ui)
 
 (require racket/sandbox)
 
@@ -12,14 +11,21 @@
 
 (provide (all-defined-out))
 
-(provide (all-from-out racket "runtime-path.rkt"))
-(provide (all-from-out rackunit rackunit/text-ui))
+(provide (all-from-out racket "runtime-path.rkt" rackunit))
 (provide (all-from-out scribble/manual scribble/eval))
 
 (define current-tamer-story (make-parameter #false))
 (define current-tamer-zone (make-parameter #false))
 
 (define /dev/null (open-output-nowhere '/dev/null #true))
+(define handbook (make-hash))
+
+(define indents (make-hash))
+(define ~indent
+  {lambda [count]
+    (unless (hash-has-key? indents count)
+      (hash-set! indents count (make-string (* count 2) #\space)))
+    (hash-ref indents count)})
 
 (define-syntax {tamer-action stx}
   (syntax-case stx []
@@ -27,13 +33,8 @@
 
 (define-syntax {tamer-require stx}
   (syntax-case stx []
-    [{_ bindname} #'(dynamic-require/expose (current-tamer-story) bindname)]
+    [{_ binding} #'(dynamic-require/expose (current-tamer-story) binding)]
     [_ #'(values curry dynamic-require/expose (current-tamer-story))]))
-
-(define-syntax {tamer-script stx}
-  (syntax-case stx []
-    [{_ bindname} #'(void (run-tests (tamer-require bindname)))]
-    [_ #'(values compose1 run-tests #'tamer-require)]))
 
 (define tamer-story->libpath
   {lambda [story-path]
@@ -58,23 +59,123 @@
                                 `(submod (file ,tamer.rkt) ,tamer-submod)
                                 (current-tamer-story)))))})
 
+(define tamer-record-handbook
+  {lambda [name action]
+    (unless (hash-has-key? handbook name)
+      (define restored #false)
+      (define-values {results cpu real gc}
+        (time-apply {thunk (parameterize ([exit-handler {lambda [status] (let* ([errmsg (format "Racket exits with status ~a!" status)])
+                                                                           (set! restored (run-test-case name {thunk (fail errmsg)})))}]
+                                          [current-output-port /dev/null]
+                                          [current-error-port /dev/null])
+                             (run-test-case name action))} null))
+      (hash-set! handbook name (cond [restored (list restored 0 0 0)]
+                                     [else (list (car results) cpu real gc)])))
+    (hash-ref handbook name)})
+
+(define tamer-script
+  {lambda suites
+    (for ([suite (in-list suites)])
+      (define ~suite (~a #:width 62 #:pad-string "." #:limit-marker "..." suite))
+      (define results (reverse (fold-test-results #:run tamer-record-handbook
+                                                  {lambda [results seed] (cons (car results) seed)}
+                                                  null
+                                                  (tamer-require suite))))
+      (cond [(andmap test-success? results) (printf "~a~a" ~suite #true)]
+            [else (for ([result (filter-not test-success? results)])
+                    (define name (test-result-test-case-name result))
+                    (cond [(test-failure? result) (let ([infos (exn:test:check-stack (test-failure-result result))])
+                                                    (eprintf "~a~a~n" ~suite #false)
+                                                    (eprintf "» FAILURE: ~a~n" name)
+                                                    (for ([info (in-list infos)])
+                                                      (eprintf "»» ~a: ~s~n"
+                                                               (check-info-name info)
+                                                               (if (symbol=? 'location (check-info-name info))
+                                                                   (cons (find-relative-path (getenv "digimon-world")
+                                                                                             (car (check-info-value info)))
+                                                                         (cdr (check-info-value info)))
+                                                                   (check-info-value info)))))]
+                          [else (let ([err (test-error-result result)])
+                                  (printf "~a~a~n" ~suite "#e")
+                                  (eprintf "» ERROR: ~a~n" name)
+                                  (eprintf "»» ~a: ~a~n"
+                                           (object-name err)
+                                           (exn-message err)))]))]))})
+
+(define tamer-spec
+  {lambda []
+    (dynamic-require (current-tamer-story) #false)
+    (parameterize ([current-namespace (module->namespace (current-tamer-story))])
+      (void (fold-test-results #:run tamer-record-handbook
+                               #:fdown {lambda [name seed] (printf "~a~a~n" (~indent seed) name) (add1 seed)}
+                               #:fup {lambda [name seed] (sub1 seed)}
+                               {lambda [results seed]
+                                 (define result (car results))
+                                 (define name (test-result-test-case-name result))
+                                 (cond [(test-success? result) (let-values ([{cpu real gc} (apply values (cdr results))])
+                                                                 (printf "~a~a [~a wallclock ms, ~a task + ~a gc = ~a CPU]~n" (~indent seed)
+                                                                         name real (- cpu gc) gc cpu))]
+                                       [(test-failure? result) (let ([infos (exn:test:check-stack (test-failure-result result))])
+                                                                 (printf "~a~a~n" (~indent seed) name)
+                                                                 (eprintf "~a» FAILURE~n" (~indent seed))
+                                                                 (for ([info (in-list infos)])
+                                                                   (eprintf (string-append (~indent seed) "»» ~a: ~s~n")
+                                                                            (check-info-name info)
+                                                                            (if (symbol=? 'location (check-info-name info))
+                                                                                (cons (find-relative-path (getenv "digimon-world")
+                                                                                                          (car (check-info-value info)))
+                                                                                      (cdr (check-info-value info)))
+                                                                                (check-info-value info)))))]
+                                       [else (let ([err (test-error-result result)])
+                                               (printf "~a~a~n" (~indent seed) name)
+                                               (eprintf "~a» ERROR~n" (~indent seed))
+                                               (eprintf "~a»» ~a: ~a~n" (~indent seed)
+                                                        (object-name err)
+                                                        (exn-message err)))])
+                                 seed}
+                               0
+                               (make-test-suite (path->string (build-path (cadadr (current-tamer-story))))
+                                                (filter test-suite?
+                                                        (filter-map {lambda [var] (namespace-variable-value var #false {lambda [] #false})}
+                                                                    (namespace-mapped-symbols)))))))})
+
 (define tamer-note
   {lambda suites
-    (parameterize ([current-output-port /dev/null]
-                   [current-error-port /dev/null])
-      (define briefs (for/fold ([briefs null]) ([suite (in-list suites)])
-                       (define status (with-handlers ([exn? (const 'undef)])
-                                        (run-tests (tamer-require suite) 'quiet)))
-                       (append briefs (list (racketidfont (symbol->string suite))
-                                            ": "
-                                            (cond [(symbol? status) (racketcommentfont (symbol->string status))]
-                                                  [(zero? status) (racketvalfont (format "~a" #true))]
-                                                  [else (racketerror (format "~a" #false))])
-                                            (linebreak)))))
-      (apply margin-note (case (length suites)
-                           [{0} (list (italic "??Testsuite Brief??"))]
-                           [{1} (list* (bold "Testsuite Brief") (linebreak) briefs)]
-                           [else (list* (bold "Testsuite Briefs") (linebreak) briefs)])))})
+    (apply margin-note
+           (for/fold ([briefs null]) ([suite (in-list suites)])
+             (append briefs (with-handlers ([exn? {lambda [e] (list (racketerror (exn-message e)))}])
+                              (car (fold-test-results #:run tamer-record-handbook
+                                                      {lambda [results seed]
+                                                        (define result (car results))
+                                                        (define-values {cpu real gc} (apply values (cdr results)))
+                                                        (define name (test-result-test-case-name result))
+                                                        (define count (cdr seed))
+                                                        (cons (append (car seed)
+                                                                      (cond [(test-success? result) (list (racketvalfont (format "~a " #true))
+                                                                                                          (racketvarfont (format "~a " count))
+                                                                                                          (racketcommentfont (format "[~ams, ~ams]" cpu real)))]
+                                                                            [(test-failure? result) (list (racketerror (format "~a " #false))
+                                                                                                          (racketvarfont (format "~a " count))
+                                                                                                          (racketcommentfont name))]
+                                                                            [else (let ([err (test-error-result result)])
+                                                                                    (list (racketerror (format "#e "))
+                                                                                          (racketvarfont (format "~a " count))
+                                                                                          (racketcommentfont (exn-message err))))])
+                                                                      (list (linebreak)))
+                                                              (add1 count))}
+                                                      (cons (list (racketidfont (format "~a" suite)) (linebreak)) 1)
+                                                      (tamer-require suite)))))))})
+
+(define stdpipe->cons
+  {lambda [routine]
+    (let ([stdout (open-output-string 'stdout)]
+          [stderr (open-output-string 'stderr)])
+      (parameterize ([current-output-port stdout]
+                     [current-error-port stderr]
+                     [exit-handler void])
+        (routine))
+      (cons (get-output-string stdout)
+            (get-output-string stderr)))})
 
 (define-binary-check {check-$? routine expected}
   (let ([status +NaN.0])
@@ -83,4 +184,9 @@
                    [current-error-port /dev/null])
       (routine))
     (check-eq? status expected (cond [(nan? status) "routine does not exit!"]
-                                     [else ""]))))
+                                     [else (format "routine exit with ~a rather than ~a!"
+                                                   status expected)]))))
+
+(define test-$?
+  {lambda [name routine expected]
+    (test-case name (check-$? routine expected))})
