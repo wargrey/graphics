2c4af36f9de9c9ac414f5cd51fc92413eb9cf71f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-03-09 17:08:16 +0800

    Update digitama: tamer.rkt: does not (time-apply) for single testcase

--- DigiGnome/digitama/tamer.rkt
+++ DigiGnome/digitama/tamer.rkt
@@ -87,10 +87,10 @@
   {lambda []
     (dynamic-require (tamer-story) #false)
     (define htag (tamer-story->tag (tamer-story)))
-    (define-values {brief cpu0 real0 gc0}
-      (prove-spec (make-test-suite htag (reverse (map cdr (hash-ref handbook-stories htag (cons '_ null)))))))
+    (define-values {brief-box cpu0 real0 gc0}
+      (time-apply prove-spec (list (make-test-suite htag (reverse (map cdr (hash-ref handbook-stories htag (cons '_ null))))))))
     (define-values {success failure error real cpu-gc gc cpu}
-      (apply values (summary-success brief) (summary-failure brief) (summary-error brief)
+      (apply values (summary-success (car brief-box)) (summary-failure (car brief-box)) (summary-error (car brief-box))
              (map {λ [t] (~r (/ t 1000.0) #:precision '{= 3})} (list real0 (- cpu0 gc0) gc0 cpu0))))
     (define population (+ success failure error))
     (cond [(zero? population) (printf "~nNo example, do not try to fool me!~n")]
@@ -103,8 +103,7 @@
   {lambda []
     (define-values {brief-box cpu0 real0 gc0}
       (time-apply {λ suites (for/fold ([brief initial-summary] [count 0]) ([suite (in-list suites)])
-                              (define-values {brief0 cpu real gc} (prove-harness suite))
-                              (values (summary** brief brief0) (add1 count)))}
+                              (values (summary** brief (prove-harness suite)) (add1 count)))}
                   (cond [(false? (tamer-story)) (hash-map handbook-stories {λ [harness stories] (make-test-suite harness (reverse (map cdr stories)))})]
                         [(module-path? (tamer-story)) (reverse (map cdr (hash-ref handbook-stories (tamer-story->tag (tamer-story)) (cons '_ null))))])))
     (define-values {success failure error real cpu-gc gc cpu}
@@ -171,7 +170,7 @@
                                     {λ _ (parameterize ([current-error-port hspec-out]
                                                         [current-output-port hspec-out])
                                            (for ([suite (in-list suite-vars)])
-                                             (define-values {brief cpu real gc} (prove-spec (tamer-require suite)))
+                                             (define brief (prove-spec (tamer-require suite)))
                                              (define-values {failure error} (values (summary-failure brief) (summary-error brief)))
                                              (unless (zero? (+ failure error))
                                                (printf "~n~a failure~a ~a error~a~n"
@@ -187,13 +186,12 @@
                                          => {λ [pieces] (and (status 0)
                                                              (echof "~a~n" line)
                                                              (racketparenfont (italic (string :bookmark:)) ~ (literal (list-ref pieces 3))))}]
-                                        [(regexp-match #px"^\\s+(.+?) (\\d+) - (.+?)( \\[(.+?)\\])?\\s*$" line)
-                                         => {λ [pieces] (let*-values ([{stts indx tm} (values (list-ref pieces 1) (list-ref pieces 2) (or (list-ref pieces 5) "-"))]
-                                                                      [{color :stts:} (if (string=? stts (~result struct:test-success))
+                                        [(regexp-match #px"^\\s+(.+?) (\\d+) - (.+?)\\s*$" line)
+                                         => {λ [pieces] (let*-values ([{color :stts:} (if (string=? (list-ref pieces 1) (~result struct:test-success))
                                                                                         (values 'green :heart:) (values 'lightred :broken-heart:))])
                                                           (status 0)
                                                           (echof #:fgcolor color "~a~n" line)
-                                                          (elem (string :stts:) (racketvarfont ~ indx) (racketresultfont ~ tm)
+                                                          (elem (string :stts:) (racketvarfont ~ (list-ref pieces 2))
                                                                 (racketcommentfont ~ (literal (list-ref pieces 3)))))}]
                                         [(regexp-match #px"^\\s*⧴ (FAILURE|FATAL) » .+?\\s*$" line)
                                          => {λ [pieces] (case (list-ref pieces 1)
@@ -223,26 +221,18 @@
   (define info-ref (get-info/full (digimon-zone)))
   
   (struct tamer-seed {datum brief name-path})
-  (struct validation {result cpu real gc})
-  (struct summary {success failure error cpu real gc})
-  (define initial-summary (summary 0 0 0 0 0 0))
+  (struct summary {success failure error})
+  (define initial-summary (summary 0 0 0))
   (define summary++
-    {lambda [summary0 record]
-      (define result (validation-result record))
+    {lambda [summary0 result]
       (summary (+ (summary-success summary0) (if (test-success? result) 1 0))
                (+ (summary-failure summary0) (if (test-failure? result) 1 0))
-               (+ (summary-error summary0) (if (test-error? result) 1 0))
-               (+ (summary-cpu summary0) (validation-cpu record))
-               (+ (summary-real summary0) (validation-real record))
-               (+ (summary-gc summary0) (validation-gc record)))})
+               (+ (summary-error summary0) (if (test-error? result) 1 0)))})
   (define summary**
     {lambda [summary1 summary2]
       (summary (+ (summary-success summary1) (summary-success summary2))
                (+ (summary-failure summary1) (summary-failure summary2))
-               (+ (summary-error summary1) (summary-error summary2))
-               (+ (summary-cpu summary1) (summary-cpu summary2))
-               (+ (summary-real summary1) (summary-real summary2))
-               (+ (summary-gc summary1) (summary-gc summary2)))})
+               (+ (summary-error summary1) (summary-error summary2)))})
   
   (define handbook-stories (make-hash))
   (define handbook-records (make-hash))
@@ -251,15 +241,14 @@
       (define short-name (car name:case«suites))
       (define long-name (string-join name:case«suites " « "))
       (unless (hash-has-key? handbook-records long-name)
-        (define-values {results cpu real gc}
-          (time-apply {λ _ (call-with-escape-continuation
-                            {λ [return] (parameterize ([current-output-port /dev/null]
-                                                       [current-error-port (open-output-string 'stderr)]
-                                                       [exit-handler {λ [status] (let ([errmsg (string-trim (get-output-string (current-error-port)))])
-                                                                                   (return (run-test-case short-name (with-check-info {{'exitcode status}}
-                                                                                                                                      {λ _ (fail errmsg)}))))}])
-                                          (return (run-test-case short-name action)))})} null))
-        (hash-set! handbook-records long-name (validation (car results) cpu real gc)))
+        (hash-set! handbook-records long-name
+                   (call-with-escape-continuation
+                    {λ [return] (parameterize ([current-output-port /dev/null]
+                                               [current-error-port (open-output-string 'stderr)]
+                                               [exit-handler {λ [status] (let ([errmsg (string-trim (get-output-string (current-error-port)))])
+                                                                           (return (run-test-case short-name (with-check-info {{'exitcode status}}
+                                                                                                                              {λ _ (fail errmsg)}))))}])
+                                  (return (run-test-case short-name action)))})))
       (hash-ref handbook-records long-name)})
   
   (define indents (make-hash))
@@ -273,13 +262,6 @@
   (define plural-suffix
     {lambda [count]
       (if (> count 1) "s" "")})
-  
-  (define ~time
-    {lambda times
-      (define-values {cpu real gc} (apply values (map (curryr / 1000.0) times)))
-      (apply format "[~a wallclock seconds (~a task + ~a gc = ~a CPU)]"
-             (map (curry ~r #:precision '{= 3})
-                  (list real (- cpu gc) gc cpu)))})
   
   (define ~result
     {lambda [result]
@@ -329,44 +311,41 @@
   
   (define fold-test-suite
     {lambda [seed:datum testsuite #:fdown [fdown default-fseed] #:fup [fup default-fseed] #:fhere [fhere default-fseed]]
-      (define-values {seed-box cpu real gc}
-        (parameterize ([current-custodian (make-custodian)])
-          (define $exn (make-parameter #false))
-          (time-apply foldts-test-suite (list {λ [testsuite name pre-action post-action seed]
-                                                (with-handlers ([exn? $exn])
-                                                  (pre-action))
-                                                (tamer-seed (fdown name (tamer-seed-datum seed))
-                                                            (tamer-seed-brief seed )
-                                                            (cons name (tamer-seed-name-path seed)))}
-                                              {λ [testsuite name pre-action post-action seed children-seed]
-                                                (with-handlers ([exn? (compose1 display-error (curry make-test-error (format "Postaction[~a]" name)))])
-                                                  (post-action))
-                                                ($exn #false)
-                                                (tamer-seed (fup name (tamer-seed-datum seed) (tamer-seed-datum children-seed))
-                                                            (tamer-seed-brief children-seed)
-                                                            (tamer-seed-name-path seed))}
-                                              {λ [testcase name action0 seed]
-                                                (define-values {name-path action}
-                                                  (cond [(exn? ($exn)) (values (cons (format "Preaction[~a]" name)
-                                                                                     (tamer-seed-name-path seed))
-                                                                               {λ _ (raise ($exn))})]
-                                                        [(false? name) (values (cons (format "(⧴ ~a)" (object-name exn:fail:user))
-                                                                                     (tamer-seed-name-path seed))
-                                                                               {λ _ (raise-user-error "Testcase should have a name")})]
-                                                        [else (values (cons name (tamer-seed-name-path seed)) action0)]))
-                                                (define record (tamer-record-handbook name-path action))
-                                                (tamer-seed (fhere record (tamer-seed-datum seed))
-                                                            (summary++ (tamer-seed-brief seed) record)
-                                                            name-path)}
-                                              (tamer-seed seed:datum initial-summary null)
-                                              testsuite))))
-      (values (tamer-seed-datum (car seed-box))
-              (tamer-seed-brief (car seed-box))
-              cpu real gc)})
+      (define seed (parameterize ([current-custodian (make-custodian)])
+                     (define $exn (make-parameter #false))
+                     (foldts-test-suite {λ [testsuite name pre-action post-action seed]
+                                          (with-handlers ([exn? $exn])
+                                            (pre-action))
+                                          (tamer-seed (fdown name (tamer-seed-datum seed))
+                                                      (tamer-seed-brief seed )
+                                                      (cons name (tamer-seed-name-path seed)))}
+                                        {λ [testsuite name pre-action post-action seed children-seed]
+                                          (with-handlers ([exn? (compose1 display-error (curry make-test-error (format "Postaction[~a]" name)))])
+                                            (post-action))
+                                          ($exn #false)
+                                          (tamer-seed (fup name (tamer-seed-datum seed) (tamer-seed-datum children-seed))
+                                                      (tamer-seed-brief children-seed)
+                                                      (tamer-seed-name-path seed))}
+                                        {λ [testcase name action0 seed]
+                                          (define-values {name-path action}
+                                            (cond [(exn? ($exn)) (values (cons (format "Preaction[~a]" name)
+                                                                               (tamer-seed-name-path seed))
+                                                                         {λ _ (raise ($exn))})]
+                                                  [(false? name) (values (cons (format "(⧴ ~a)" (object-name exn:fail:user))
+                                                                               (tamer-seed-name-path seed))
+                                                                         {λ _ (raise-user-error "Testcase should have a name")})]
+                                                  [else (values (cons name (tamer-seed-name-path seed)) action0)]))
+                                          (define record (tamer-record-handbook name-path action))
+                                          (tamer-seed (fhere record (tamer-seed-datum seed))
+                                                      (summary++ (tamer-seed-brief seed) record)
+                                                      name-path)}
+                                        (tamer-seed seed:datum initial-summary null)
+                                        testsuite)))
+      (values (tamer-seed-datum seed) (tamer-seed-brief seed))})
   
   (define prove-spec
     {lambda [suite]
-      (define-values {whocares brief cpu real gc}
+      (define-values {whocares brief}
         (fold-test-suite #:fdown {λ [name seed:ordered]
                                    (if (null? seed:ordered)
                                        (echof #:fgcolor 202 #:attributes '{underline} "λ ~a~a~n" (~indent (length seed:ordered)) name)
@@ -376,12 +355,10 @@
                                  (cond [(null? seed:ordered) null]
                                        [else (cons (add1 (car seed:ordered))
                                                    (cdr seed:ordered))])}
-                         #:fhere {λ [record seed:ordered]
-                                   (define result (validation-result record))
+                         #:fhere {λ [result seed:ordered]
                                    (define headline (format "~a~a ~a - " (~indent (length seed:ordered)) (~result result) (car seed:ordered)))
                                    (define headspace (~indent (string-length headline) #:times 1))
-                                   (cond [(test-success? result) (void (echof #:fgcolor 'green "~a~a" headline (test-result-test-case-name result))
-                                                                       (echof #:fgcolor 245 " [~ams]~n" (validation-real record)))]
+                                   (cond [(test-success? result) (void (echof #:fgcolor 'green "~a~a~n" headline (test-result-test-case-name result)))]
                                          [(test-failure? result) (void (echof #:fgcolor 'lightred "~a~a~n" headline (test-result-test-case-name result))
                                                                        (display-failure result #:indent headspace))]
                                          [(test-error? result) (void (echof #:fgcolor 'red "~a~a~n" headline (test-result-test-case-name result))
@@ -390,16 +367,13 @@
                                    (cons (add1 (car seed:ordered)) (cdr seed:ordered))}
                          null
                          suite))
-      (values brief cpu real gc)})
+      (values brief)})
   
   (define prove-harness
     {lambda [suite]
-      (define-values {$?≠0 brief cpu real gc}
-        (fold-test-suite #:fhere {λ [record seed:$?≠0] (let ([result (validation-result record)])
-                                                         (cond [(test-success? result) seed:$?≠0]
-                                                               [else (cons result seed:$?≠0)]))}
-                         null
-                         suite))
+      (define-values {$?≠0 brief}
+        (fold-test-suite null suite #:fhere {λ [result seed:$?≠0] (cond [(test-success? result) seed:$?≠0]
+                                                                        [else (cons result seed:$?≠0)])}))
       (echof "~a" (~a #:width 64 #:pad-string "." #:limit-marker "..." (rackunit-test-suite-name suite)))
       (cond [(positive? (summary-error brief)) (echof #:fgcolor 'red "~a~n" (~result struct:test-error))]
             [(positive? (summary-failure brief)) (echof #:fgcolor 'lightred "~a~n" (~result struct:test-failure))]
@@ -409,7 +383,6 @@
         (cond [(test-failure? result) (display-failure result)]
               [(test-error? result) (display-error result)]
               [else (error "RackUnit has new test result type added!")]))
-      (when (null? $?≠0) (echof #:fgcolor 245 "~a~n" (~time (summary-cpu brief) (summary-real brief) (summary-gc brief))))
-      (values brief cpu real gc)})}
+      (values brief)})}
 
 (require (submod "." digitama))
