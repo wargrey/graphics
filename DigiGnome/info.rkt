a3551e970d616ff94831915055027db255f86d0f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-04-02 22:05:03 +0800

    Update DigiGnome: info.rkt: only omit (digimon-stone) when compiling

--- DigiGnome/info.rkt
+++ DigiGnome/info.rkt
@@ -6,7 +6,7 @@
 
 (define build-deps '{"base" "at-exp-lib" "scribble-lib"})
 
-(define compile-omit-paths (list "makefile.rkt" "submake.rkt" "info.rkt" "stone" "tamer"))
+(define compile-omit-paths (list "stone"))
 (define test-omit-paths 'all)
 
 (define pkg-desc "The Meta-Project built for developers to make life simple")
