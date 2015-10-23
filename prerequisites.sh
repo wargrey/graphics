d0712b86a9b0fde876e9212578770a908bcce440
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-10-23 23:04:17 +0800

    Update Mission: prerequisites.sh: download racket source from my github

--- prerequisites.sh
+++ prerequisites.sh
@@ -26,30 +26,26 @@ export LDFLAGS := -L/usr/local/opt/readline/lib -L/usr/local/opt/gmp/lib
 export CPPFLAGS := -I/usr/cal/opt/readline/include -I/usr/local/opt/gmp/include
 
 Prefix := /opt
-VerRegExp := \([0-9]\+\.\)\+[0-9]\+
 
 racket_dir := PLTracket
 racket_bin0 := $(shell which racket)
 racket_bin := $(if $(findstring 0,$(words $(racket_bin0))),$(Prefix)/$(racket_dir)/bin/racket,$(racket_bin0))
-racket_verion := $(shell curl --compressed http://download.racket-lang.org/all-versions.html | grep -m 1 -o 'racket-minimal-v$(VerRegExp)' | tr -d a-z-)
-racket_src := racket-minimal-$(racket_verion)-src-builtpkgs.tgz
 racket_pkgs_linux := libfontconfig libcairo2 libpango1.0 libjpeg62 freeglut3 libncurses5-dev
 racket_pkgs := math html make images pict3d net-cookies
 racket_config_solaris := --enable-places --enable-futures --disable-libffi
 racket_config_darwin := --enable-macprefix
 racket_config_linux :=
 
-$(racket_src):
-	curl -O "http://mirror.racket-lang.org/installers/$(racket_verion)/$(racket_src)"
+$(racket_dir):
+	cd /tmp && git clone github.com:wargrey/racket.git $(racket_dir)
 
-$(Prefix)/$(racket_dir)/bin/racket: $(racket_src)
-	rm -fr racket-$(racket_verion) && tar xzvf $(racket_src)
-	cd racket-$(racket_verion)/src && ./configure --prefix=$(Prefix)/$(racket_dir) --enable-origtree $(racket_config_$(OSNAME))
-	cd racket-$(racket_verion)/src && $(MAKE) -j$(CPUS)
-	cd racket-$(racket_verion)/src && sudo $(MAKE) install
+$(Prefix)/$(racket_dir)/bin/racket: $(racket_dir)
+	cd $(racket_verion)/racket/src && ./configure --prefix=$(Prefix)/$(racket_dir) --enable-origtree $(racket_config_$(OSNAME))
+	cd $(racket_verion)/racket/src && $(MAKE) -j$(CPUS)
+	cd $(racket_verion)/racket/src && sudo $(MAKE) install
 
 racket: $(if $(findstring 0,$(words $(racket_bin0))),$(racket_bin))
-	test -n "$(racket_pkgs_$(OSNAME))" && sudo apt-get install --yes $(racket_pkgs_$(OSNAME))
+	test -n "$(racket_pkgs_$(OSNAME))" && sudo pkgin install -y $(racket_pkgs_$(OSNAME)) # TODO: support other OSes
 	sudo $(addprefix $(dir $(racket_bin)),raco) pkg install --skip-installed --scope installation --deps search-auto $(racket_pkgs)
 	sudo $(addprefix $(dir $(racket_bin)),raco) pkg install --jobs 1 --skip-installed --scope installation --deps search-auto whalesong
 	$(racket_bin) -l racket/base -e '(printf "We have Racket ~a.~n" (version))'
