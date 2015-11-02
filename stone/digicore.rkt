ff69c32a5c343338d80ef1084033448778a7da99
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-11-03 05:30:26 +0800

    Update stone: digicore.rkt: move (provider)s before (require)s

--- stone/digicore.rkt
+++ stone/digicore.rkt
@@ -4,9 +4,9 @@
 
 #lang typed/racket
 
-(require "@(getenv "digicore.rkt")")
-
 (provide (all-defined-out))
 (provide (all-from-out "@(getenv "digicore.rkt")"))
+
+(require "@(getenv "digicore.rkt")")
 
 (current-digimon "@(current-digimon)")
