86428edc6acefb653a1917389378b34791066829
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2015-12-08 01:14:37 +0800

    Update tamer: tamer.rkt: set locale tongue to English

--- tamer/tamer.rkt
+++ tamer/tamer.rkt
@@ -1,8 +1,12 @@
 #lang at-exp racket/base
 
-;;; To force makefile.rkt counting the required file
-@require{../digitama/tamer.rkt}
-
 (provide (all-defined-out))
 (provide (except-out (all-from-out "../digitama/tamer.rkt")
                      exn:break:hang-up? exn:break:terminate?))
+
+;;; To force makefile.rkt counting the required file
+@require{../digitama/tamer.rkt}
+@require{../digitama/i18n.rkt}
+
+(current-tongue 'English)
+
