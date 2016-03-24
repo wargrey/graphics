999bebc207df905a9fa422142412beaeb2079557
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-03-24 14:55:45 +0800

    Update digitama: ugly.rkt: add (define/make-is-a?)

--- digitama/ugly.rkt
+++ digitama/ugly.rkt
@@ -1,6 +1,10 @@
 #lang typed/racket
 
+(provide (all-defined-out))
+
 (require typed/racket/unsafe)
+
+(require (for-syntax racket/syntax))
 
 (define-syntax (unsafe-require/typed/provide stx)
   (syntax-case stx []
@@ -15,13 +19,15 @@
   
   (define make-is-a?
     (lambda [c]
-      (λ [v] (is-a? v c))))
-
-  (define make-subclass?
-    (lambda [c]
-      (λ [v] (subclass? v c)))))
+      (λ [v] (is-a? v c)))))
 
 (unsafe-require/typed/provide
  (submod "." ugly)
- [make-is-a? (All (%) (-> % (-> Any Boolean : #:+ (Instance %))))]
- [make-subclass? (All (%) (-> % (-> Any Boolean : #:+ %)))])
+ [make-is-a? (All (%) (-> % (-> Any Boolean : #:+ (Instance %))))])
+
+(define-syntax (define/make-is-a? stx)
+  (syntax-case stx [: class]
+    [(_ % : Type% (class s-exp ...))
+     (with-syntax ([%? (format-id #'% "~a?" (syntax-e #'%))])
+       #'(begin (define % : Type% (class s-exp ...))
+                (define %? ((inst make-is-a? Type%) %))))]))
