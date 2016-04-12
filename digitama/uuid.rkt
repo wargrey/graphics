08ae98fee530bd9b42c75fae9f17145d5e3d58d9
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-04-13 00:10:34 +0800

    Update digitama: uuid.rkt: simplify the implementation

--- digitama/uuid.rkt
+++ digitama/uuid.rkt
@@ -6,24 +6,14 @@
 
 (provide uuid:random)
 
-(define-type Racket-Place-Status
-  (Vector Nonnegative-Fixnum Nonnegative-Fixnum Nonnegative-Fixnum
-          Natural Natural Natural Natural Natural Fixnum Fixnum Natural Natural))
-
-(require/typed racket/base
-               [vector-set-performance-stats! (-> Racket-Place-Status (Option Thread) Void)])
-
 (define clock-sequence-semaphore : Semaphore (make-semaphore 1))
-(define pseudo-random-numbers : Racket-Place-Status (vector 0 0 0 0 0 0 0 0 0 0 0 0))
 
 (define version+timestamp : (-> (U 1 2 3 4 5) String)
   (lambda [version]
-    (vector-set-performance-stats! pseudo-random-numbers #false)
-    (define process-ms : Nonnegative-Fixnum (vector-ref pseudo-random-numbers 0))
     (define actually-us : Natural (max (exact-round (* (current-inexact-milliseconds) 1000)) 0))
-    (define simulated-ns : Natural (remainder process-ms 1000))
-    (define ts : String (~a (format "~x" (+ (* actually-us 1000) simulated-ns))
-                            #:align 'right #:width 15 #:left-pad-string "0"))
+    (define simulated-ns : Natural (remainder (max (current-gc-milliseconds) 0) 1000))
+    (define ts : String (~a #:align 'right #:width 15 #:left-pad-string "0"
+                            (format "~x" (+ (* actually-us 1000) simulated-ns))))
     (define time-low : String (substring ts 7 15))
     (define time-mid : String (substring ts 3 7))
     (define time-high : String (substring ts 0 3))
@@ -32,32 +22,33 @@
 (define variant+clock-sequence : (-> Natural)
   (lambda []
     (dynamic-wind (thunk (semaphore-wait clock-sequence-semaphore))
-                  (thunk (let ([gctime (vector-ref pseudo-random-numbers 2)]
-                               [place-gcount (vector-ref pseudo-random-numbers 3)]
-                               [thd-switches (vector-ref pseudo-random-numbers 4)]
-                               [memory (+ (vector-ref pseudo-random-numbers 10) (current-memory-use))])
-                           (+ #b1000000000000000 #| ensure the N is 8, 9, A, or B |#
-                              (remainder (+ gctime place-gcount thd-switches memory)
-                                         #b11111111111111))))
+                  (thunk (+ #b1000000000000000 #| ensure the N is 8, 9, A, or B |#
+                            (remainder (current-memory-use) #b11111111111111)))
                   (thunk (semaphore-post clock-sequence-semaphore)))))
 
 (define uuid:random : (-> String)
   (lambda []
+    (define pr+gc : Fixnum (current-process-milliseconds))
+    (define gc : Fixnum (current-gc-milliseconds))
     (format "~a-~x-~a~a" (version+timestamp 4) (variant+clock-sequence)
-            (~a (format "~x" (bitwise-and #xffffff (vector-ref pseudo-random-numbers 8)))
-                #:align 'right #:width 6 #:left-pad-string "0")
-            (~a (format "~x" (bitwise-and #xffffff (vector-ref pseudo-random-numbers 9)))
-                #:align 'right #:width 6 #:left-pad-string "0"))))
+            (~a #:align 'right #:width 5 #:left-pad-string "0" (format "~x" (bitwise-and #xfffff gc)))
+            (~a #:align 'right #:width 7 #:left-pad-string "0" (format "~x" (bitwise-and #xfffffff pr+gc))))))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (module+ test
   (define uuids : (HashTable String Integer) (make-hash))
+  
   (for ([i (in-range 64)])
     (define uuid : String (uuid:random))
     (hash-set! uuids uuid (add1 (hash-ref uuids uuid (const 0)))))
 
-  (exit (for/fold ([errno : Natural 0]) ([(uuid count) (in-hash uuids)])
-          (printf "~a: ~a~n" uuid count)
-          (if (= count 1) errno (add1 errno)))))
+  (define errno : Natural
+    (for/fold ([errno : Natural 0]) ([(uuid count) (in-hash uuids)])
+      (printf "~a: ~a~n" uuid count)
+      (if (= count 1) errno (add1 errno))))
+
+  (unless (zero? errno)
+    (printf "~a duplicates~n" errno)
+    (exit errno)))
