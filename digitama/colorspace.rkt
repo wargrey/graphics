d7d468ae52532f4fd0efcba358e0fdf2c37d22a6
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-08-30 13:03:53 +0800

    Update digitama: colorspace.rkt: remove example data of C and C2

--- digitama/colorspace.rkt
+++ digitama/colorspace.rkt
@@ -10,19 +10,6 @@
 (define-type HSB->RGB (-> Hue Gamut Gamut (Values Gamut Gamut Gamut)))
 (define-type RGB->HSB (-> Gamut Gamut Gamut (Values Hue Gamut Gamut)))
 
-(define rgb-bytes->hsb : (-> RGB->HSB Byte Byte Byte (Values Hue Gamut Gamut))
-  (lambda [rgb->hsb red green blue]
-    (rgb->hsb (fl (/ red   #xFF))
-              (fl (/ green #xFF))
-              (fl (/ blue  #xFF)))))
-
-(define hsb->rgb-bytes : (-> HSB->RGB Real Real Real (Values Byte Byte Byte))
-  (lambda [hsb->rgb hue s% b%]
-    (define-values (red green blue) (hsb->rgb (real->hue hue) (real->gamut s%) (real->gamut b%)))
-    (values (gamut->byte red)
-            (gamut->byte green)
-            (gamut->byte blue))))
-
 (define gamut->byte : (-> Gamut Byte) (λ [v] (min (exact-round (fl* v 255.0)) #xFF)))
 (define real->gamut : (-> Real Gamut) (λ [v] (flmax (flmin (fl v) 1.0) 0.0)))
 
@@ -35,6 +22,19 @@
                                 [(positive? hue) (flabs (fl (+ integer-part (- hue (truncate hue)))))]
                                 [(zero? integer-part) (flabs (fl+ 360.0 (fl (- hue (truncate hue)))))]
                                 [else (flabs (fl (- integer-part (- (truncate hue) hue))))]))])))
+
+(define rgb-bytes->hsb : (-> RGB->HSB Byte Byte Byte (Values Hue Gamut Gamut))
+  (lambda [rgb->hsb red green blue]
+    (rgb->hsb (fl (/ red   #xFF))
+              (fl (/ green #xFF))
+              (fl (/ blue  #xFF)))))
+
+(define hsb->rgb-bytes : (-> HSB->RGB Real Real Real (Values Byte Byte Byte))
+  (lambda [hsb->rgb hue s% b%]
+    (define-values (red green blue) (hsb->rgb (real->hue hue) (real->gamut s%) (real->gamut b%)))
+    (values (gamut->byte red)
+            (gamut->byte green)
+            (gamut->byte blue))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define hsv->rgb : HSB->RGB
@@ -169,26 +169,26 @@
                     (ormap (λ [[m : Flonum]] (eq? (exact-round (fl* src m)) (exact-round (fl* r m))))
                            (list 1000.0 100.0 10.0)))])))
 
-  (define examples : (Listof (List Natural Gamut Gamut Gamut Hue Hue Gamut Gamut Gamut Gamut Gamut Gamut Gamut Gamut Gamut))
-    (list (list #xFFFFFF  1.000  1.000  1.000  +nan.0  +nan.0  0.000  0.000  1.000  1.000  1.000  1.000  0.000  0.000  0.000)
-          (list #x808080  0.500  0.500  0.500  +nan.0  +nan.0  0.000  0.000  0.500  0.500  0.500  0.500  0.000  0.000  0.000)
-          (list #x000000  0.000  0.000  0.000  +nan.0  +nan.0  0.000  0.000  0.000  0.000  0.000  0.000  0.000  0.000  0.000)
-          (list #xFF0000  1.000  0.000  0.000  0.0     0.0     1.000  1.000  1.000  0.500  0.333  0.299  1.000  1.000  1.000)
-          (list #xBFBF00  0.750  0.750  0.000  60.0    60.0    0.750  0.750  0.750  0.375  0.500  0.664  1.000  1.000  1.000)
-          (list #x008000  0.000  0.500  0.000  120.0   120.0   0.500  0.500  0.500  0.250  0.167  0.293  1.000  1.000  1.000)
-          (list #x80FFFF  0.500  1.000  1.000  180.0   180.0   0.500  0.500  1.000  0.750  0.833  0.850  0.500  1.000  0.400)
-          (list #x8080FF  0.500  0.500  1.000  240.0   240.0   0.500  0.500  1.000  0.750  0.667  0.557  0.500  1.000  0.250)
-          (list #xBF40BF  0.750  0.250  0.750  300.0   300.0   0.500  0.500  0.750  0.500  0.583  0.457  0.667  0.500  0.571)
-          (list #xA0A424  0.628  0.643  0.142  61.8    61.5    0.501  0.494  0.643  0.393  0.471  0.581  0.779  0.638  0.699)
-          (list #x411BEA  0.255  0.104  0.918  251.1   250.0   0.814  0.750  0.918  0.511  0.426  0.242  0.887  0.832  0.756)
-          (list #x1EAC41  0.116  0.675  0.255  134.9   133.8   0.559  0.504  0.675  0.396  0.349  0.460  0.828  0.707  0.667)
-          (list #xF0C80E  0.941  0.785  0.053  49.5    50.5    0.888  0.821  0.941  0.497  0.593  0.748  0.944  0.893  0.911)
-          (list #xB430E5  0.704  0.187  0.897  283.7   284.8   0.710  0.636  0.897  0.542  0.596  0.423  0.792  0.775  0.686)
-          (list #xED7651  0.931  0.463  0.316  14.3    13.2    0.615  0.556  0.931  0.624  0.570  0.586  0.661  0.817  0.446)
-          (list #xFEF888  0.998  0.974  0.532  56.9    57.4    0.466  0.454  0.998  0.765  0.835  0.931  0.467  0.991  0.363)
-          (list #x19CB97  0.099  0.795  0.591  162.4   163.4   0.696  0.620  0.795  0.447  0.495  0.564  0.875  0.779  0.800)
-          (list #x362698  0.211  0.149  0.597  248.3   247.3   0.448  0.420  0.597  0.373  0.319  0.219  0.750  0.601  0.533)
-          (list #x7E7EB8  0.495  0.493  0.721  240.5   240.4   0.228  0.227  0.721  0.607  0.570  0.520  0.316  0.290  0.135)))
+  (define examples : (Listof (List Natural Gamut Gamut Gamut Hue Hue Gamut Gamut Gamut Gamut Gamut Gamut Gamut))
+    (list (list #xFFFFFF  1.000  1.000  1.000  +nan.0  +nan.0  1.000  1.000  1.000  1.000  0.000  0.000  0.000)
+          (list #x808080  0.500  0.500  0.500  +nan.0  +nan.0  0.500  0.500  0.500  0.500  0.000  0.000  0.000)
+          (list #x000000  0.000  0.000  0.000  +nan.0  +nan.0  0.000  0.000  0.000  0.000  0.000  0.000  0.000)
+          (list #xFF0000  1.000  0.000  0.000  0.0     0.0     1.000  0.500  0.333  0.299  1.000  1.000  1.000)
+          (list #xBFBF00  0.750  0.750  0.000  60.0    60.0    0.750  0.375  0.500  0.664  1.000  1.000  1.000)
+          (list #x008000  0.000  0.500  0.000  120.0   120.0   0.500  0.250  0.167  0.293  1.000  1.000  1.000)
+          (list #x80FFFF  0.500  1.000  1.000  180.0   180.0   1.000  0.750  0.833  0.850  0.500  1.000  0.400)
+          (list #x8080FF  0.500  0.500  1.000  240.0   240.0   1.000  0.750  0.667  0.557  0.500  1.000  0.250)
+          (list #xBF40BF  0.750  0.250  0.750  300.0   300.0   0.750  0.500  0.583  0.457  0.667  0.500  0.571)
+          (list #xA0A424  0.628  0.643  0.142  61.8    61.5    0.643  0.393  0.471  0.581  0.779  0.638  0.699)
+          (list #x411BEA  0.255  0.104  0.918  251.1   250.0   0.918  0.511  0.426  0.242  0.887  0.832  0.756)
+          (list #x1EAC41  0.116  0.675  0.255  134.9   133.8   0.675  0.396  0.349  0.460  0.828  0.707  0.667)
+          (list #xF0C80E  0.941  0.785  0.053  49.5    50.5    0.941  0.497  0.593  0.748  0.944  0.893  0.911)
+          (list #xB430E5  0.704  0.187  0.897  283.7   284.8   0.897  0.542  0.596  0.423  0.792  0.775  0.686)
+          (list #xED7651  0.931  0.463  0.316  14.3    13.2    0.931  0.624  0.570  0.586  0.661  0.817  0.446)
+          (list #xFEF888  0.998  0.974  0.532  56.9    57.4    0.998  0.765  0.835  0.931  0.467  0.991  0.363)
+          (list #x19CB97  0.099  0.795  0.591  162.4   163.4   0.795  0.447  0.495  0.564  0.875  0.779  0.800)
+          (list #x362698  0.211  0.149  0.597  248.3   247.3   0.597  0.373  0.319  0.219  0.750  0.601  0.533)
+          (list #x7E7EB8  0.495  0.493  0.721  240.5   240.4   0.721  0.607  0.570  0.520  0.316  0.290  0.135)))
   
   (for/list : (Listof Datum) ([tc (in-list examples)])
     (define-values (color-name metrics) (values (color->name (first tc)) (take-right (cdr tc) 7)))
