a9931aef7917ce34c66dba1e9b54947fe76b0889
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-10 00:40:58 +0800

    Bugfix: Mission: dependencies should be put in package's info.rkt

--- bitmap/info.rkt
+++ bitmap/info.rkt
@@ -1,11 +1,6 @@
 #lang info
 
 (define collection 'use-pkg-name)
-(define pkg-desc "Functional Bitmaps")
-
-(define deps '("base" "digimon" "images-lib" "typed-racket-lib"))
-
 (define version "1.0")
-(define pkg-authors '(wargrey))
 
 (define scribblings '(["tamer/bitmap.scrbl" (main-doc) (gui-library)]))
