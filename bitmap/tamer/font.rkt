ff2d96821cee1d7fd58fc71602f173c39e51d2a7
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-04-18 01:18:51 +0800

    typed racket is more reliable

--- bitmap/tamer/font.rkt
+++ bitmap/tamer/font.rkt
@@ -1,25 +1,25 @@
-#lang racket
+#lang typed/racket
 
 (require "../main.rkt")
 
 (default-css-pen (make-css-pen #:color 'gray #:width 1 #:style 'solid))
 
-(define bitmap-text*
+(define bitmap-text* : (-> String (Instance Font%) Bitmap)
   (lambda [text font]
     (bitmap-frame #:border 'gray
                   (bitmap-text #:ascent 'Magenta #:descent 'Blue #:capline 'Orange #:meanline 'Green #:baseline 'Red
                                text (make-css-font (make-css-font font #:size 'xx-large) #:size 2f0)))))
 
-#;(for/list ([face (in-list (get-face-list))])
+#;(for/list : (Listof (Pairof String Bitmap)) ([face (in-list (get-face-list))])
     (cons face (bitmap-vl-append (bitmap-text* "Sphinx[+]" (make-css-font #:family face #:ligature 'normal))
                                  (bitmap-text* "Sphinx[-]" (make-css-font #:family face #:ligature 'none)))))
 
 (bitmap-vl-append* #:gapsize 16
- (for/list ([face (in-list (list "Qomolangma-Uchen Sarchung" "Microsoft Himalaya" "Kailasa"))])
+ (for/list : (Listof Bitmap) ([face (in-list (list "Qomolangma-Uchen Sarchung" "Microsoft Himalaya" "Kailasa"))])
    (bitmap-vl-append (bitmap-text* (string-append face ": བོད་ཡིག[+]") (make-css-font #:family face #:ligature 'normal))
                      (bitmap-text* (string-append face ": བོད་ཡིག[-]") (make-css-font #:family face #:ligature 'none)))))
 
 (bitmap-vl-append* #:gapsize 16
- (for/list ([family (in-list (list 'decorative 'roman 'script 'swiss 'modern 'symbol 'system))])
+ (for/list : (Listof Bitmap) ([family (in-list (list 'decorative 'roman 'script 'swiss 'modern 'symbol 'system))])
    (define font (make-css-font #:family family #:ligature 'normal))
    (bitmap-text* (format "~a[~a]: norm -> Sphinx" (send font get-family) (send font get-face)) font)))
