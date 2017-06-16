cc14d003187272b9f7e2a907b096b6d973f98c97
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-06-16 21:50:29 +0800

    add draw.rkt, but usually it should not be used

---
+++ bitmap/draw.rkt
@@ -0,0 +1,20 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+(provide (all-from-out typed/racket/draw))
+
+(require typed/racket/draw)
+
+(require "digitama/digicore.rkt")
+(require "constructor.rkt")
+
+(define bitmap-dc : (->* ((-> (Instance Bitmap-DC%) Nonnegative-Flonum Nonnegative-Flonum Any))
+                         (Nonnegative-Real (Option Nonnegative-Real) Positive-Real)
+                         Bitmap)
+  (lambda [draw-with [w 0] [h #false] [density (default-bitmap-density)]]
+    (define bmp : Bitmap (bitmap-blank w h density))
+    (define dc : (Instance Bitmap-DC%) (send bmp make-dc))
+    (send dc set-smoothing 'aligned)
+    ;;; let the device deal with non-integer size
+    (draw-with dc (real->double-flonum w) (real->double-flonum (or h w)))
+    bmp))
