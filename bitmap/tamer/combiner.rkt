da148c277e4e03533f91dc562b8b5ecf2fd345f3
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-06-26 21:24:53 +0800

    mark benchmark of tamer/combiner.rkt

--- bitmap/tamer/combiner.rkt
+++ bitmap/tamer/combiner.rkt
@@ -1,26 +1,27 @@
 #lang typed/racket
 
-(require "../digitama/digicore.rkt")
+(require "../digitama/draw.rkt")
 (require "../constructor.rkt")
 (require "../composite.rkt")
 (require "../font.rkt")
+(require "../color.rkt")
 
 (define examples : (Listof Bitmap)
   (list (bitmap-text "Field Name: ")    (bitmap-text "Testcase for (bitmap-table)")
         (bitmap-text "Specification: ") (bitmap-paragraph "* Shown in a table of 2 rows;\n* This Field has no examples." #:max-width 300)
         (bitmap-text "Examples: ")))
 
-(define modern : Font (make-css-font #:family 'modern))
+(define monospace : Font (desc-font #:family 'monospace))
 (define bitmaps : (Listof Bitmap)
   (for/list : (Listof Bitmap) ([x (in-range 1000)])
     (define rc : Integer (random #xFFFFFF))
-    (bitmap-text (string-upcase (format "~x" rc)) modern #:color rc)))
+    (bitmap-text (string-upcase (format "~x" rc)) monospace #:color (rgb* rc))))
 
 (collect-garbage)
 (time (bitmap-table 2 examples '(rc lc) '(ct) '(8) '(8)))
 
-(collect-garbage)
+(collect-garbage) ; 35±3ms
 (time (void (bitmap-vl-append* bitmaps)))
 
-(collect-garbage)
+(collect-garbage) ; 40±1ms
 (time (bitmap-table 26 bitmaps '(rc) '(cc) '(10) '(10)))
