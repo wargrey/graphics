83f318b28349428e57a77fee1211e2c26fcfa82b
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-08-11 21:34:56 +0800

    add new enum pattern

--- bitmap/digitama/enumeration.rkt
+++ bitmap/digitama/enumeration.rkt
@@ -3,6 +3,7 @@
 (provide (all-defined-out))
 
 (require (for-syntax racket/base))
+(require (for-syntax racket/string))
 (require (for-syntax racket/syntax))
 (require (for-syntax racket/sequence))
 (require (for-syntax syntax/parse))
@@ -34,17 +35,25 @@
      #'(begin (define-enumeration id : TypeU [enum ...])
               (define kw-filter : (case-> [TypeU Args ... -> Type] [Symbol Args ... -> (Option Type)])
                 (lambda [kw args ...] (case kw [(enum) sexp ...] ... [else #false]))))]
-    [(_ id #:+> TypeU kw->enum enum->kw #:-> Type [enum value] ... [enum$ value$])
+    [(_ id #:+> TypeU kw->enum enum->kw [enum:id value:integer] ...)
+     (with-syntax ([expected (datum->syntax #'enum->kw (string-join (map number->string (syntax->datum #'(value ...)))
+                                                                    ", " #:before-first "{" #:after-last "}"))])
+       #'(begin (define-enumeration id : TypeU #:with kw->enum #:-> Integer [enum value] ...)
+                (define enum->kw : (All (a) (->* (Integer) ((-> Integer a)) (U TypeU a)))
+                  (lambda [kv [on-error (λ [[given : Integer]] (raise-argument-error 'id expected given))]]
+                    (cond [(= kv value) 'enum] ... [else (on-error kv)])))))]
+    [(_ id #:+> TypeU kw->enum enum->kw [start:integer enum ... enum$])
+     (with-syntax* ([(value ... value$) (for/list ([<enum> (in-syntax #'(enum ... enum$))] [idx (in-naturals 0)])
+                                          (datum->syntax <enum> (+ (syntax-e #'start) idx)))]
+                    [expected (datum->syntax #'enum->kw (format "[~a, ~a]" (syntax-e #'start) (syntax-e #'value$)))])
+       #'(begin (define-enumeration id : TypeU #:with kw->enum #:-> Integer [enum value] ... [enum$ value$])
+                (define enum->kw : (All (a) (->* (Integer) ((-> Integer a)) (U TypeU a)))
+                  (lambda [kv [on-error (λ [[given : Integer]] (raise-argument-error 'id expected given))]]
+                    (cond [(= kv value) 'enum] ... [(= kv value$) 'enum$] [else (on-error kv)])))))]
+    [(_ id #:+> TypeU kw->enum enum->kw #:range Type [enum value] ... [enum$ value$])
      (with-syntax ([(range ...) (for/list ([<start> (in-syntax #'(value ...))]
                                            [<end> (sequence-tail (in-syntax #'(value ... value$)) 1)])
                                   (datum->syntax <start> (/ (+ (syntax-e <start>) (syntax-e <end>)) 2)))])
        #'(begin (define-enumeration id : TypeU #:with kw->enum #:-> Type [enum value] ... [enum$ value$])
                 (define (enum->kw [kv : Type]) : TypeU
-                  (cond [(< kv range) 'enum] ... [else 'enum$]))))]
-    [(_ id #:+> TypeU kw->enum enum->kw [start:integer enum ... enum$])
-     (with-syntax ([(value ... value$) (for/list ([<enum> (in-syntax #'(enum ... enum$))]
-                                                  [idx (in-naturals 0)])
-                                         (datum->syntax <enum> (+ (syntax-e #'start) idx)))])
-       #'(begin (define-enumeration id : TypeU #:with kw->enum #:-> Integer [enum value] ... [enum$ value$])
-                (define (enum->kw [kv : Integer]) : TypeU
-                  (cond [(= kv value) 'enum] ... [else 'enum$]))))]))
+                  (cond [(< kv range) 'enum] ... [else 'enum$]))))]))
