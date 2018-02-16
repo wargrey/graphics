c96374f8703dfa51731afdf1de83d7a5d7b7ceab
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-02-16 22:20:34 +0800

    refactoring huffman compression

--- png/digitama/huffman.rkt
+++ png/digitama/huffman.rkt
@@ -1,13 +1,15 @@
 #lang typed/racket/base
 
 ;;; https://en.wikipedia.org/wiki/Huffman_coding
+;;; http://rosettacode.org/wiki/Huffman_coding#Racket
 
-(provide Huffman-Node Huffman-Dictionary)
-(provide make-huffman-tree huffman-tree->dictionary)
+(provide Huffman-Node Huffman-Dictionary Huffman-Encoder)
+(provide make-huffman-tree huffman-tree->dictionary make-huffman-encoder)
 
 (require racket/unsafe/ops)
 
 (define-type Huffman-Node (U Internal Leaf))
+(define-type Huffman-Encoder (->* (Bytes) (Index Index) (Pairof Natural Index)))
 (define-type Huffman-Dictionary (HashTable Byte (Pairof Integer Integer)))
 
 (struct Node ([weight : Index]) #:transparent)
@@ -17,23 +19,15 @@
 (define make-huffman-tree : (->* (Bytes) (Index Index) Huffman-Node)
   (lambda [src [idx0 0] [idxn 0]]
     (define end : Index (if (<= idxn idx0) (bytes-length src) idxn))
-    ;;;;;;;;;;;; this is much faster than manually inserting via list. ;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;; this is much faster than manually inserting via lists ;;;;;;;;;;;;;;;;;;;;;;
     (define symbols : (HashTable Byte Integer) (make-hasheq))
     (for ([b (in-bytes src idx0 end)]) (hash-update! symbols b add1 generate-zero-when-not-present))
     (define nodes : (Listof Huffman-Node)
       (for/fold ([nodes : (Listof Huffman-Node) null])
                 ([(symbol weight) (in-hash symbols)])
         (insert-node nodes (Leaf (assert weight index?) symbol))))
-    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    (cond [(null? nodes) (Leaf 0 0)]
-          [else (let tree ([tail : (Listof Huffman-Node) nodes])
-                  (let ([ch0 : Huffman-Node (unsafe-car tail)]
-                        [ch1.tail : (Listof Huffman-Node) (unsafe-cdr tail)])
-                    (cond [(null? ch1.tail) ch0]
-                          [else (let ([ch1 (car ch1.tail)])
-                                  (tree (insert-node (cdr ch1.tail)
-                                                     (Internal (assert (+ (Node-weight ch0) (Node-weight ch1)) index?)
-                                                               ch0 ch1))))])))])))
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    (huffman-nodes->tree nodes)))
 
 (define huffman-tree->dictionary : (-> Huffman-Node Huffman-Dictionary)
   (lambda [tree]
@@ -50,10 +44,42 @@
               [else (hash-set! symbols (Leaf-symbol node) (cons code deep))])))
     symbols))
 
+(define make-huffman-encoder : (-> Huffman-Node Huffman-Encoder)
+  (lambda [tree]
+    (define symbols : Huffman-Dictionary (huffman-tree->dictionary tree))
+    (λ [[src : Bytes] [idx0 0] [idxn 0]] : (Pairof Natural Index)
+      (define end : Index (if (<= idxn idx0) (bytes-length src) idxn))
+      (let encode ([code : Integer 0]
+                   [total : Integer 0]
+                   [idx : Integer idx0])
+        (cond [(unsafe-fx= idx end) (cons (assert code exact-nonnegative-integer?) (assert total index?))]
+              [else (let ([b : Byte (bytes-ref src idx)])
+                      (define code.deep : (Pairof Integer Integer) (hash-ref symbols b break-when-symbol-not-found))
+                      (define len : Integer (cdr code.deep))
+                      (encode (bitwise-ior (arithmetic-shift code len) (car code.deep))
+                              (unsafe-fx+ total len)
+                              (unsafe-fx+ idx 1)))])))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define generate-zero-when-not-present : (-> Zero)
   (lambda []
     0))
+
+(define break-when-symbol-not-found : (-> Nothing)
+  (lambda []
+    (error 'huffman "unmatched dictionary")))
+
+(define huffman-nodes->tree : (-> (Listof Huffman-Node) Huffman-Node)
+  (lambda [nodes]
+    (cond [(null? nodes) (Leaf 0 0)]
+          [else (let tree ([tail : (Listof Huffman-Node) nodes])
+                  (let ([ch0 : Huffman-Node (unsafe-car tail)]
+                        [ch1.tail : (Listof Huffman-Node) (unsafe-cdr tail)])
+                    (cond [(null? ch1.tail) ch0]
+                          [else (let ([ch1 (car ch1.tail)])
+                                  (tree (insert-node (cdr ch1.tail)
+                                                     (Internal (assert (+ (Node-weight ch0) (Node-weight ch1)) index?)
+                                                               ch0 ch1))))])))])))
 
 (define insert-node : (-> (Listof Huffman-Node) Huffman-Node (Listof Huffman-Node))
   (lambda [nodes node]
--- png/tamer/huffman.rkt
+++ png/tamer/huffman.rkt
@@ -14,10 +14,23 @@
               (~r #:base 2 #:min-width (cdr value) #:pad-string "0"
                   (car value))))))
 
-(define src (file->bytes (simplify-path (build-path (find-system-path 'orig-dir) (find-system-path 'run-file)))))
+(define wiki-src #"A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED")
+(define this-src (file->bytes (simplify-path (build-path (find-system-path 'orig-dir) (find-system-path 'run-file)))))
 
-(define wiki-test (time (huffman-tree->dictionary (make-huffman-tree #"A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"))))
-(define this-test (time (huffman-tree->dictionary (make-huffman-tree src))))
+(define wiki-test (time (huffman-tree->dictionary (make-huffman-tree wiki-src))))
+(define this-test (time (huffman-tree->dictionary (make-huffman-tree this-src))))
 
 (display-huffman-dictionary wiki-test "Testcase form Wikipedia")
 (display-huffman-dictionary this-test "Testcase of the sources")
+
+(define wiki-encoder (time (make-huffman-encoder (make-huffman-tree wiki-src))))
+(define this-encoder (time (make-huffman-encoder (make-huffman-tree this-src))))
+
+(define wiki-huffman (time (wiki-encoder wiki-src)))
+(define this-huffman (time (this-encoder this-src)))
+
+(cons (number->string (car wiki-huffman) 2) (cdr wiki-huffman))
+(cons (number->string (car this-huffman) 2) (cdr this-huffman))
+
+(integer-length (car wiki-huffman))
+(integer-length (car this-huffman))
