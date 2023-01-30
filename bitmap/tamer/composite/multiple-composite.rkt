1ef71dead83418339c6da8dd3cc26d45f6290cba
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-01-30 13:22:09 +0800

    add examples of making ring chart

--- bitmap/tamer/composite/multiple-composite.rkt
+++ bitmap/tamer/composite/multiple-composite.rkt
@@ -4,9 +4,11 @@
 (require "../../composite.rkt")
 (require "../../font.rkt")
 (require "../../color.rkt")
+(require "../../paint.rkt")
 
-(require "../../digitama/unsafe/convert.rkt")
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define ring-thickness : Flonum 32.0)
+(define ring-colors : (Listof Symbol) '(royalblue crimson lime purple chocolate khaki))
 (define monospace : Font (desc-font #:family 'monospace #:size 16.0))
 
 (bitmap-vl-append* #:gapsize -8.0
@@ -16,10 +18,32 @@
                                  (string-upcase (format "~a: ~x" (add1 i) rc))
                                  monospace)))))
 
-(bitmap-cc-superimpose*
- (build-list 4 (λ [[i : Index]]
+(bitmap-cc-superimpose
+ 
+ (bitmap-cc-superimpose*
+  (build-list 4 (λ [[i : Index]]
                   (let ([rc (random #xFFFFFF)]
                         [fs (* 8.0 (add1 i))])
                     (bitmap-text #:color (rgb* rc)
                                  (string-upcase (format "~a" fs))
                                  (desc-font #:family 'monospace #:size fs))))))
+
+ (bitmap-cc-superimpose*
+  (build-list (length ring-colors)
+              (λ [[i : Index]]
+                (let ([arclength (/ 360.0 (length ring-colors))])
+                  (bitmap-arc #:stroke (desc-stroke #:width ring-thickness #:color (list-ref ring-colors i))
+                              #:radian? #false
+                              (* ring-thickness 4.0)
+                              (* i arclength)
+                              (* (+ i 1) arclength)))))))
+
+(bitmap-cc-superimpose*
+ (build-list (length ring-colors)
+             (λ [[i : Index]]
+                 (let ([arclength (/ 360.0 (length ring-colors))])
+                   (bitmap-sector #:fill (list-ref ring-colors i) #:border 'ghostwhite
+                                  #:radian? #false
+                                  (* ring-thickness 4.0)
+                                  (* i arclength)
+                                  (* (+ i 1) arclength))))))
