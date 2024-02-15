fd0ca72558c49202b3ed41dfb28c81d3fe2aba1a
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-02-15 10:57:57 +0800

    omit all tests in root collection

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define collection 'multi)
 (define pkg-desc "Essential Computer Graphics for Typed Racket")
 (define pkg-authors '(wargrey))
+(define test-omit-paths 'all)
 
 (define version "1.0")
 (define deps '("base" "digimon" "math-lib" "draw-lib" "typed-racket-lib" "typed-racket-more"))
