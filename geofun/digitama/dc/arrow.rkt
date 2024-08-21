03628803bd99fefc5087c4f3040dc4b295fdb8ea
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-08-22 00:55:11 +0800

    finish `arrow.rkt`

---
+++ geofun/digitama/dc/arrow.rkt
@@ -0,0 +1,64 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require digimon/metrics)
+
+(require "paint.rkt")
+(require "../../paint.rkt")
+
+(require "../convert.rkt")
+(require "../unsafe/dc/shape.rkt")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(struct geo:arrow geo
+  ([head-radius : Nonnegative-Flonum]
+   [shaft-length : Nonnegative-Flonum]
+   [shaft-thickness : Nonnegative-Flonum]
+   [start-angle : Flonum]
+   [wing-angle : (Option Flonum)])
+  #:type-name Geo:Arrow
+  #:transparent)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define geo-arrow : (->* (Real Real)
+                         (Real #:id (Option Symbol) #:border Maybe-Stroke-Paint #:fill Maybe-Fill-Paint
+                               #:shaft-thickness Real #:wing-angle (Option Real) #:radian? Boolean)
+                         Geo:Arrow)
+  (lambda [#:id [id #false] #:shaft-thickness [shaft-thickness -0.3] #:wing-angle [wing-angle #false]
+           #:radian? [radian? #true] #:border [border (void)] #:fill [pattern (void)]
+           head-radius shaft-length [start 0.0]]
+    (define rhead : Nonnegative-Flonum (~length head-radius))
+    (define shaft-flthickness : Nonnegative-Flonum (~length shaft-thickness rhead))
+    (define shaft-flength : Nonnegative-Flonum (~length shaft-length rhead))
+    (define wing-flangle (and wing-angle (~radian wing-angle radian?)))
+    ;(define arrow-bbox : Geo-Calculate-BBox (geo-shape-plain-bbox (+ d flength) d))
+    
+    (create-geometry-object geo:arrow
+                            #:with [(geo-shape-surface-wrapper geo-arrow-surface border pattern)] #:id id
+                            rhead shaft-flength shaft-flthickness (~radian start radian?) wing-flangle)))
+
+(define geo-arrowhead : (->* (Real)
+                             (Real #:id (Option Symbol) #:border Maybe-Stroke-Paint #:fill Maybe-Fill-Paint
+                                   #:shaft-thickness Real #:wing-angle (Option Real) #:radian? Boolean)
+                             Geo:Arrow)
+  (lambda [#:id [id #false] #:border [border (void)] #:fill [pattern (void)]
+           #:shaft-thickness [shaft-thickness 0.0] #:wing-angle [wing-angle #false] #:radian? [radian? #true]
+           radius [start 0.0]]
+    (define flradius : Nonnegative-Flonum (~length radius))
+    (define shaft-flthickness : Nonnegative-Flonum (~length shaft-thickness flradius))
+    (define wing-flangle (and wing-angle (~radian wing-angle radian?)))
+    (define dart-bbox : Geo-Calculate-BBox (geo-shape-plain-bbox (* 2.0 flradius)))
+    
+    (create-geometry-object geo:arrow
+                            #:with [(geo-shape-surface-wrapper geo-arrow-surface border pattern) dart-bbox] #:id id
+                            flradius 0.0 shaft-flthickness (~radian start radian?) wing-flangle)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define geo-arrow-surface : Geo-Surface-Create
+  (lambda [self]
+    (with-asserts ([self geo:arrow?])
+      (dc_arrow create-abstract-surface
+                (geo:arrow-head-radius self) (geo:arrow-start-angle self)
+                (current-border-source) (current-fill-source) (default-geometry-density)
+                (geo:arrow-shaft-thickness self) (geo:arrow-shaft-length self) (geo:arrow-wing-angle self)))))
