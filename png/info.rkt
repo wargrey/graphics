fbec6c8ef1664b76e680e9fe05ce45fd2b0c03c1
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2026-03-10 21:44:32 +0800

    hide docs of png

--- png/info.rkt
+++ png/info.rkt
@@ -3,14 +3,11 @@
 (define collection 'use-pkg-name)
 (define pkg-desc "PNG: Read and Write Portable Network Graphics")
 
-(define deps '("base" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc"))
-
 (define version "0.0.1.0")
 (define pkg-authors '(wargrey))
 (define test-omit-paths 'all)
 
-(define msvc-sdk-root "C:\\Program Files (x86)\\Windows Kits\\10")
-(define msvc-sdk-library "10.0.17763.0")
+(define deps '("base" "typed-racket-lib" "typed-racket-more"))
+(define build-deps '("scribble-lib" "racket-doc"))
 
-(define scribblings '(["tamer/png.scrbl" (main-doc) (parsing-library)]))
+;(define scribblings '(["tamer/png.scrbl" (main-doc) (parsing-library)]))
